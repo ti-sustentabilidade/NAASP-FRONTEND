2107526741ec843afb9b7c61c6a9cfb14d922541
Pedro Henrique Oliveira Siqueira <ph13633@gmail.com>
2024-06-16 21:06:17 -0300

    adjust family getAll route

--- src/constants/api-routes.ts
+++ src/constants/api-routes.ts
@@ -1,7 +1,9 @@
 // Define e exporta o objeto API_Routes
 export const API_Routes = {
-  NAASP: { // Objeto que contém as rotas da API para o serviço NAASP
-    users: { // Objeto que contém as rotas da API para o recurso de usuários
+  NAASP: {
+    // Objeto que contém as rotas da API para o serviço NAASP
+    users: {
+      // Objeto que contém as rotas da API para o recurso de usuários
       login: "/login", // Rota para o endpoint de login
       password_reset: "/solicitar-nova-senha", // Rota para o endpoint de redefinição de senha
       getAll: "/", // Rota para o endpoint que retorna todos os usuários
@@ -11,7 +13,7 @@ export const API_Routes = {
       delete: "/", // Rota para o endpoint que exclui um usuário existente
     },
     family: {
-      getAll: "/familias",
+      getAll: "/buscarTodasFamilias",
       create: "/novaFamilia",
     },
   },
